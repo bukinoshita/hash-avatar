90027cdb63a4aa188dc499571c9e69d2e39e2c21
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-18 21:02:57 -0400

    Hash Avatar

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # hash-avatar
+
+> Hash avatar algorithm
+
+## Install
+```
+$ npm install --save hash-avatar
+```
+
+## Usage
+```js
+import hashAvatar from 'hash-avatar'
+
+hashAvatar('uid', { size: 100 })
+// => It will return a SVG
+```
+
+## API
+### hashAvatar(uid, { size })
+
+#### uid
+Type: `string`<br/>
+Required
+
+#### size
+Type: `number`<br/>
+Required
+
+## Related
+
+- [react-hash-avatar](https://github.com/bukinoshita/react-hash-avatar) — React hash avatar algorithm
+
+## License
+[MIT](https://github.com/bukinoshita/hash-avatar/blob/master/LICENSE) &copy; Bu Kinoshita
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const hash = require('string-hash')
+const color = require('tinycolor2')
+
+module.exports = (uid, { size } = {}) => {
+  const n = hash(uid)
+  const c1 = color({ h: n % 360, s: 0.95, l: 0.5 })
+  const c1_ = c1.toHexString()
+  const c2 = c1.triad()[1].toHexString()
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <svg width="${size}px" height="${size}px" viewBox="0 0 80 80" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="g">
+        <stop stop-color="${c1_}" offset="0%"></stop>
+        <stop stop-color="${c2}" offset="100%"></stop>
+      </linearGradient>
+    </defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <rect id="Rectangle" fill="url(#g)" x="0" y="0" width="80" height="80"></rect>
+    </g>
+  </svg>`
+}
